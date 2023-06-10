d30bfc113c0647bf5aceae26d52eee11be6dccae
A-Finger <anthony.palchyk@gmail.com>
2023-06-10 10:55:37 +0300

    Implemented business logic

---
+++ SingleResponsibility.BL/Sevices/UserAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using SingleResponsibility.DAL.Models;
+using SingleResponsibility.DAL.Repositories;
+
+namespace SingleResponsibility.BL.Sevices
+{
+	public class UserAuthenticationService
+	{
+		private readonly UserRepository userRepository;
+        public UserAuthenticationService(UserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+		public bool AuthenticateUser(string username, string password)
+		{
+			User user = userRepository.GetUserByUsername(username);
+			return user is not null && user.Password == password;
+		}
+	}
+}
---
+++ SingleResponsibility.BL/Sevices/UserRegistrationService.cs
@@ -0,0 +1,21 @@
+﻿using SingleResponsibility.DAL.Models;
+using SingleResponsibility.DAL.Repositories;
+
+namespace SingleResponsibility.BL.Sevices
+{
+	public class UserRegistrationService
+	{
+		public readonly UserRepository userRepository;
+		public UserRegistrationService(UserRepository userRepository)
+		{
+			this.userRepository = userRepository;
+		}
+
+		public void RegisterUser(string name, string password)
+		{
+			User user = userRepository.GetUserByUsername(name) is null ?
+				throw new ArgumentException("This user already exists") :
+				new User(name, password);
+		}
+	}
+}
